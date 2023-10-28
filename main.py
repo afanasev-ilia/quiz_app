e4331674282513d7c70f8032778d0701e3289558
Илья Афанасьев <iafanasevmail@gmail.com>
2023-10-28 22:11:09 +0300

    added aiohttp.ClientSession

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import requests
-
+import aiohttp
 from fastapi import FastAPI
+from fastapi_asyncalchemy import db
+from sqlalchemy.sql import exists
 
 from schema import Question_num
 
@@ -8,9 +9,19 @@ app = FastAPI()
 
 
 @app.post('/questions')
-def questions(item: Question_num):
+async def questions(item: Question_num):
     question_num = item.question_num
-    response = requests.get(
-        f'https://jservice.io/api/random?count={question_num}',
-    )
-    print(response.text)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+                f"https://jservice.io/api/random?count={question_num}"
+        ) as resp:
+            api_data = await resp.json()
+            for model in api_data:
+                while db.session.query(exists().where(model.question_id == model["id"])).scalar():
+                    print(
+                        f'id {model["id"]} with question: "{model["question"]}" '
+                        f'is already exists in database!'
+                    )
+                    async with session.get(f'https://jservice.io/api/random') as api_res:
+                        new_question = await api_res.json()
+                        model = new_question[0]
