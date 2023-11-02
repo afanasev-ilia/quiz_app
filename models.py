32eebd725465f4fce716aba0fbb92cbd732a4daa
Илья Афанасьев <iafanasevmail@gmail.com>
2023-11-02 23:26:13 +0300

    changed models

--- models.py
+++ models.py
@@ -2,15 +2,30 @@ from datetime import datetime
 
 from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP
 
-metadata = MetaData()
+
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
+# metadata = MetaData()
+
+Base = declarative_base()
 
 
-question = Table(
-    'question',
-    metadata,
-    Column('id', Integer, primary_key=True),
-    Column('question_id', Integer),
-    Column('question', String),
-    Column('answer', String),
-    Column('created_at', TIMESTAMP, default=datetime.utcnow),
-)
+class Question(Base):
+    __tablename__ = "question"
+    id = Column(Integer, primary_key=True, index=True)
+    question_id = Column(Integer)
+    question = Column(String)
+    answer = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+
+# question = Table(
+#     'question',
+#     metadata,
+#     Column('id', Integer, primary_key=True),
+#     Column('question_id', Integer),
+#     Column('question', String),
+#     Column('answer', String),
+#     Column('created_at', TIMESTAMP, default=datetime.utcnow),
+# )
