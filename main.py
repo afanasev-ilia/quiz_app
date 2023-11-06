9e6903037c013f457d77c982aaff111f285915a4
Илья Афанасьев <iafanasevmail@gmail.com>
2023-11-06 22:33:25 +0300

    changed models

--- main.py
+++ main.py
@@ -19,19 +19,20 @@ def get_last_question(model: Question = Question) -> Question:
 @app.post('/questions')
 async def questions(item: Question_num):
     last = get_last_question()
-    question_num = item.question_num
-    async with aiohttp.ClientSession() as session:
-        async with session.get(
-                f"https://jservice.io/api/random?count={question_num}"
-        ) as resp:
-            api_data = await resp.json()
-            for model in api_data:
-                while db.session.query(exists().where(model.question_id == model["id"])).scalar():
-                    print(
-                        f'id {model["id"]} with question: "{model["question"]}"'
-                        f'is already exists in database!'
-                    )
-                    async with session.get(f'https://jservice.io/api/random') as api_res:
-                        new_question = await api_res.json()
-                        model = new_question[0]
-    return last.model if last else []
+    return last
+    # question_num = item.question_num
+    # async with aiohttp.ClientSession() as session:
+    #     async with session.get(
+    #             f"https://jservice.io/api/random?count={question_num}"
+    #     ) as resp:
+    #         api_data = await resp.json()
+    #         for model in api_data:
+    #             while db.session.query(exists().where(model.question_id == model["id"])).scalar():
+    #                 print(
+    #                     f'id {model["id"]} with question: "{model["question"]}"'
+    #                     f'is already exists in database!'
+    #                 )
+    #                 async with session.get(f'https://jservice.io/api/random') as api_res:
+    #                     new_question = await api_res.json()
+    #                     model = new_question[0]
+    # return last.model if last else []
